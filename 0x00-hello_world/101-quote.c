39d5af7f0827bcb4b84219bb284b7803ef1ac59a
Daniel <asante.danulo@gmail.com>
2023-03-16 14:14:16 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stderr.h>
+/**
+ * main -Printing a code in standard error
+ * Return: 1
+ */
+
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
