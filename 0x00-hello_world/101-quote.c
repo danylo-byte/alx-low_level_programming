bc42efd15153530183955f3aa8d07bf94a4b9315
Daniel <asante.danulo@gmail.com>
2023-03-16 14:37:18 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<stderr.h>
+#include<unistd.h>
 /**
  * main -Printing a code in standard error
  * Return: 1
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 return (0);
 }
