89e7410f694af03bc101a24f2f70042e4f2a5629
Daniel <asante.danulo@gmail.com>
2023-03-17 16:45:09 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,10 @@
-
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
-/**                                                                                                         
- * main -Last digit.                                                                                        
- * Return: 0                                                                                                
- */
+/**
+* main -Last digit.
+* Return: 0
+*/
 int main(void)
 {
 int n;
