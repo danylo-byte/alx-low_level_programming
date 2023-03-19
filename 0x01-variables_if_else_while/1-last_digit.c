3c8315629517fdb810ebfebf81816baf2944995a
Daniel <asante.danulo@gmail.com>
2023-03-19 07:37:34 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
 }
 else if (n == 0 || l_digit == 0)
 {
-printf("last digit of %d is 0 and is 0\n", n);
+printf("Last digit of %d is 0 and is 0\n", n);
 }
 else if (n < 6 && n != 0)
 {
